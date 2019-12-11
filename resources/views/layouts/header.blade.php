8de7c60f825ba76b7917abd4e7fccbb2c3c85d26
Sovannak22 <beng.sovannak@gmail.com>
2019-12-11 21:17:57 +0700

    user management v2

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,20 +1,13 @@
 <header class="main-header">
-        <!-- Logo -->
     <a href="{{url('')}}" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><img class="img-rounded" src="{{ asset('sitephoto.jpg') }}" style="width:75%" alt=""></span>
-        <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><img class="img-rounded" src="{{ asset('sitephoto.jpg') }}" style="width:15%" alt=""><b>Where</b> 2 Go</span>
     </a>
-    <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-        <!-- Sidebar toggle button-->
         <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
         </a>
-        <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
-
             <ul class="nav navbar-nav">
                 <li>
                     <a href="#"
