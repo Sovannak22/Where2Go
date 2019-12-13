62b2861e0fae20aa320ff4533cc5fdb764432a16
mavin19 <koung.mavin@gmail.com>
2019-12-13 14:46:04 +0700

    sent image to server not done

--- resources/views/event/create_event.blade.php
+++ resources/views/event/create_event.blade.php
@@ -16,11 +16,11 @@
             <div class="box-header with-border" style="padding:30px">
                 <h3 class="box-title">Create Event</h3>
             </div>
-            <form action="{{url('events/create')}}" style="padding:30px" method="POST">
+            <form id="event_upload" style="padding:30px" method="POST" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <div class="form-group">
                 <label for="title">Title:</label>
-                <input type="text" name="title" placeholder="write your event title here" class="form-control" id="title" required>
+                <input type="text" name="title" placeholder="write your event title here" class="form-control" id="title">
                 </div>
                 <div class="form-group">
                 <label for="include">Include:</label>
@@ -28,11 +28,11 @@
                 </div>
                 <div class="form-group">
                     <label for="duration">Duration:</label>
-                    <input type="text"name="duration" placeholder="enter your duration here" class="form-control" id="Duration" required>
+                    <input type="text"name="duration" placeholder="enter your duration here" class="form-control" id="Duration">
                 </div>
                 <div class="form-group">
                     <label for="categories">Categories:</label>
-                    <select class="form-control" name="categories_id" id="sel1" required>
+                    <select class="form-control" name="categories_id" id="sel1">
                         @foreach ($categories as $item)
                         <option value="{{$item->id}}">{{$item->category}}</option>
                         @endforeach  
@@ -40,27 +40,27 @@
                 </div>
                 <div class="form-group">
                     <label for="price">Price:</label>
-                    <input type="text" name="price" placeholder="enter your price here" class="form-control" id="price" required>
+                    <input type="text" name="price" placeholder="enter your price here" class="form-control" id="price">
                 </div>
                 <div class="form-group">
                     <label for="limit">Limit:</label>
-                    <input type="text" name="limitation" placeholder="enter your limit here" class="form-control" id="limitation" required>
+                    <input type="text" name="limitation" placeholder="enter your limit here" class="form-control" id="limitation">
                 </div>     
                 <div class="form-group">
                     <label for="start_date">Start Date:</label>
-                    <input type="date" name="start_date" class="form-control" id="start_date" required>
+                    <input type="date" name="start_date" class="form-control" id="start_date">
                 </div>
                 <div class="form-group">
                     <label for="end_date">End Date:</label>
-                    <input type="date" name="end_date" class="form-control" id="end_date" required>
+                    <input type="date" name="end_date" class="form-control" id="end_date">
                 </div>
                 <div class="form-group">
                     <label for="contact">Contact:</label>
-                    <input type="phone" name="contact" placeholder="080 888 888" class="form-control" id="contact" required>
+                    <input type="phone" name="contact" placeholder="080 888 888" class="form-control" id="contact">
                 </div>
                 <div class="form-group">
                     <label for="transpotation">Transpotation:</label>
-                    <input type="text" name="transpotation" placeholder="bus, moto..." class="form-control" id="transpotation" required>
+                    <input type="text" name="transpotation" placeholder="bus, moto..." class="form-control" id="transpotation">
                 </div>
                 <div class="form-group">
                     <label for="location">Location:</label>
@@ -68,12 +68,12 @@
                 </div>
                 <div class="form-group">
                     <label for="inputPassword3">Description:</label>
-                    <textarea name="description" placeholder="write your event summery here" class="form-control" id="" cols="30" rows="10" required></textarea>
+                    <textarea name="description" placeholder="write your event summery here" class="form-control" id="" cols="30" rows="10"></textarea>
                 </div>
                 <div class="form-group">
                         <fieldset class="form-group">
                             <a href="javascript:void(0)" onclick="$('#pro-image').click()">Upload Image</a>
-                            <input type="file" id="pro-image" name="pro-image" style="display: none;" class="form-control" multiple >
+                            <input type="file" id="pro-image" name="pro-image" class="form-control" multiple >
                         </fieldset>
                         <div class="preview-images-zone">
                             
